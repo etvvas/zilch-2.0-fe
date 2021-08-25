db858cd6ab9f9960434963199932e44f0be8cfcf
Tis Lais <tistististis@gmail.com>
2021-08-25 13:45:23 -0700

    Add avatars to AuthForm

--- src/components/auth/AuthForm.jsx
+++ src/components/auth/AuthForm.jsx
@@ -27,11 +27,51 @@ const AuthForm = () => {
             className={input}
             placeholder="Password" />
         </label>
+
+        {/* Sign up && */}
+        <div className="flex flex-wrap gap-4 justify-center my-10">
+          <svg className={svg}>
+            <use href={avatars + '#bear'} />
+          </svg>
+          <svg className={svg}>
+            <use href={avatars + '#dog'} />
+          </svg>
+          <svg className={svg}>
+            <use href={avatars + '#polar-bear'} />
+          </svg>
+          <svg className={svg}>
+            <use href={avatars + '#puffer-fish'} />
+          </svg>
+          <svg className={svg}>
+            <use href={avatars + '#dog-1'} />
+          </svg>
+          <svg className={svg}>
+            <use href={avatars + '#weasel'} />
+          </svg>
+          <svg className={svg}>
+            <use href={avatars + '#giraffe'} />
+          </svg>
+          <svg className={svg}>
+            <use href={avatars + '#cobra'} />
+          </svg>
+          <svg className={svg}>
+            <use href={avatars + '#cow'} />
+          </svg>
+        </div>
+
         {/* Toggle text Log in / Sign up */}
         <button className={button}>Log in</button>
       </form>
+
+      <div className="text-xs text-center text-gray-400">Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+    </div>
   )
 }
+
+const svg = `
+  w-20
+  h-20
+`;
 
 const wrap = `
   mx-auto
