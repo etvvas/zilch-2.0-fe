81be6820ab488ac4aba74d59651677a650c0a560
Tis Lais <tistististis@gmail.com>
2021-08-25 13:44:33 -0700

    Add layout elements to AuthForm

--- src/components/auth/AuthForm.jsx
+++ src/components/auth/AuthForm.jsx
@@ -4,17 +4,60 @@ import avatars from '../../assets/avatars.svg';
 const AuthForm = () => {
 
   return (
+    <div className={wrap}>
+
+      {/* Toggle text Log in / Sign up */}
+      <h1 className={h1}>Log in</h1>
+      
       <form className={form}>
+
+
+        
+
+        {/* Log in */}
+        <label className={label}>
+          <span className={span}>Username</span>
           <input 
             className={input}
             placeholder="Username" />
+        </label>
+        <label className={label}>
+          <span className={span}>Password</span>
           <input 
             className={input}
             placeholder="Password" />
+        </label>
+        {/* Toggle text Log in / Sign up */}
         <button className={button}>Log in</button>
       </form>
   )
 }
+
+const wrap = `
+  mx-auto
+  max-w-xs
+`;
+
+const h1 = `
+  text-4xl
+  text-center
+  font-light
+  my-10
+`;
+
+const label = `
+  flex
+  flex-col
+  my-2
+`;
+
+const span = `
+  font-bold
+  uppercase
+  text-sm
+  text-gray-700
+  mb-2
+`;
 
 const input = `
   border
