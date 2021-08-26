ce9e04e055c376fa05215daa9b80ccee87cf0f2c
Gabriel Simek <gabrielsimek@outlook.com>
2021-08-26 00:57:12 -0700

    add PrivateRoute component

---
+++ src/components/common/PrivateRoute.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Redirect, Route} from 'react-router-dom'
+import { useSession } from '../../state/SessionProvider';
+const PrivateRoute = (props) => {
+    const session = useSession()
+    if(!session) return <Redirect to="/" />
+    return <Route {...props} />
+}
+
+export default PrivateRoute;
