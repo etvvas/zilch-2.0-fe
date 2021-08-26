d0b132afa736321c51809377724d1169323d9ad1
Tis Lais <tistististis@gmail.com>
2021-08-26 09:36:04 -0700

    Add more style

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { io } from 'socket.io-client';
 import Header from './components/header/Header';
 import AuthForm from './components/auth/AuthForm';
 import { Switch, Route } from 'react-router-dom';
+import Navigation from './components/header/Navigation';
 
 io('https://zilch-v2-staging.herokuapp.com/')
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Header />
+      <Navigation />
 
       <Switch>
         <Route path="/" exact={true} component={AuthForm} />
