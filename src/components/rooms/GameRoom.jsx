8a607058e04b9fcea9ab3008c2ca9aabf78d245b
Katherine Tam <katherinemtam@gmail.com>
2021-08-26 15:48:27 -0700

    grab session and socket in GameRoom

--- src/components/rooms/GameRoom.jsx
+++ src/components/rooms/GameRoom.jsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSession } from '../../state/SessionProvider';
+import { socket, SocketContext } from '../../state/SocketProvider';
 
 const GameRoom = () => {
+  //grab user session info 
+  const session = useSession();;
+
+  //grab params
+  const {room} = useParams();
+
+  //grab socket instance
+  const socket = useContext(SocketContext);
+
+  //useEffect that will trigger join event
+    
+  socket.emit('JOIN_GAME', session, room);
+  useEffect(()=> {
+
+    socket.on('WELCOME', msg => {
+    console.log(msg)
+    })
+  })
 
   
   return (
