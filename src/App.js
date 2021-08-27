2f22c193a9278d81e1cd7ed668a79051039a1b96
Tis Lais <43742374+tislais@users.noreply.github.com>
2021-08-27 13:35:16 -0700

    Game page layout (#13)
    * Start adding elements to game page
    
    * Add basic game page layout
    
    * Separate game page into smaller components
    
    * Fix incorrect merge
    
    * Add redirects file
    
    * Fix imports

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 // import { io } from 'socket.io-client';
 import Header from './components/header/Header';
 import AuthForm from './components/auth/AuthForm';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import Lobby from './components/rooms/Lobby';
 import GameRoom from './components/rooms/GameRoom';
+import PrivateRoute from './components/common/PrivateRoute';
 // import SocketProvider from './state/SocketProvider';
 import {SocketContext, socket} from './state/SocketProvider'
 function App() {
@@ -14,10 +15,10 @@ function App() {
       <Header />
 
       <Switch>
-        <Route path="/" exact={true} component={AuthForm} />
+        <PrivateRoute path="/" exact={true} component={AuthForm} />
         <SocketContext.Provider value={socket}>
-        <Route path="/lobby" exact component={Lobby} />
-        <Route path="/lobby/:room" exact component={GameRoom} />
+        <PrivateRoute path="/lobby" exact component={Lobby} />
+        <PrivateRoute path="/lobby/:room" exact component={GameRoom} />
         </SocketContext.Provider>
       </Switch>
     </>
--- src/components/rooms/GameRoom.jsx
+++ src/components/rooms/GameRoom.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import ActiveScoreboard from '../game/ActiveScoreboard';
+import Dice from '../game/Dice';
+import GameControls from '../game/GameControls';
+import PlayerProgress from '../game/PlayerProgress';
+import ScoringOptions from '../game/ScoringOptions';
 
 const GameRoom = () => {
 
@@ -7,58 +12,23 @@ const GameRoom = () => {
     <div className={wrap}>
       <h1 className={h1}>ScrumMast3r vs _UXgUrU_</h1>
 
-      <div className={scoreBoard}>
-        <div className={currentPlayerBox}>
-          <p className={currentPlayer}>Your move, ZilchPlayer420</p>
-          <p className={currentPlayerScore}>6669 pts</p>
-        </div>
-        <table>
-          <thead>
-            <tr>
-              <td>Round</td>
-              <td>Total</td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>500</td>
-              <td>6669</td>
-            </tr>
-            <tr>
-              <td>500</td>
-              <td>6669</td>
-            </tr>
-            <tr>
-              <td>500</td>
-              <td>6669</td>
-            </tr>
-            <tr>
-              <td>500</td>
-              <td>6669</td>
-            </tr>
-          </tbody>
-        </table>
+
+      <ActiveScoreboard />
+
+      <div className={mainControls}>
+        <Dice />
+        <GameControls />
       </div>
-    <div className={mainControls}>
-      <div className={diceDisplay}>
-        <i className={die + 'fa-dice-one'}></i>
-        <i className={die + 'fa-dice-two'}></i>
-        <i className={die + 'fa-dice-three'}></i>
-        <i className={die + 'fa-dice-four'}></i>
-        <i className={die + 'fa-dice-five'}></i>
-        <i className={die + 'fa-dice-six'}></i>
-      </div>
-      <div className={diceControls}>
-        <button className={diceButton + rollButton}>Roll</button>
-        <button className={diceButton + bankButton}>Bank</button>
-      </div>
-    </div>
+
+      <ScoringOptions />
+      <PlayerProgress />
+
     </div>
   )
 }
 
 const wrap = `
-  max-w-screen-xl
+  max-w-screen-sm
   mx-auto
   p-4
 `;
@@ -66,64 +36,12 @@ const wrap = `
 const h1 = `
   text-2xl
   text-center
-`;
-
-const scoreBoard = `
-  grid
-  grid-cols-2
-`;
-
-const currentPlayerBox = `
-  flex
-  flex-col
-`;
-
-const currentPlayer = `
-  text-lg
-`;
-
-const currentPlayerScore = `
-  text-3xl
+  my-10
 `;
 
 const mainControls = `
 
 `;
 
-const diceDisplay = `
-  flex
-  gap-2
-  justify-between
-  my-4
-`;
-
-const die = `
-  fas
-  text-6xl
-  text-gray-700
-`;
-
-const diceControls = `
-  flex
-  flex-row
-  gap-4
-  my-4
-`;
-
-const diceButton = `
-  py-4
-  text-center
-  text-white
-  w-full
-  rounded
-`;
-
-const rollButton = `
-  bg-indigo-500
-`;
-
-const bankButton = `
-  bg-green-500
-`;
 
 export default GameRoom;
