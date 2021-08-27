1d6bfcbb4a6becc3f5a0c3200cc2a74604e2d58b
Tis Lais <tistististis@gmail.com>
2021-08-27 13:29:20 -0700

    Add redirects file

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AuthForm from './components/auth/AuthForm';
 import { Switch, Route } from 'react-router-dom';
 import Lobby from './components/rooms/Lobby';
 import GameRoom from './components/rooms/GameRoom';
+import PrivateRoute from './components/common/PrivateRoute';
 // import SocketProvider from './state/SocketProvider';
 import {SocketContext, socket} from './state/SocketProvider'
 function App() {
@@ -14,10 +15,10 @@ function App() {
       <Header />
 
       <Switch>
-        <Route path="/" exact={true} component={AuthForm} />
+        <PrivateRoute path="/" exact={true} component={AuthForm} />
         <SocketContext.Provider value={socket}>
-        <Route path="/lobby" exact component={Lobby} />
-        <Route path="/lobby/:room" exact component={GameRoom} />
+        <PrivateRoute path="/lobby" exact component={Lobby} />
+        <PrivateRoute path="/lobby/:room" exact component={GameRoom} />
         </SocketContext.Provider>
       </Switch>
     </>
