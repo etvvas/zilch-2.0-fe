0025b19378d65486c3523f53c895686635e05078
Tis Lais <43742374+tislais@users.noreply.github.com>
2021-08-27 15:31:49 -0700

    Game page layout - controls and dice (#16)
    * Start adding elements to game page
    
    * Add basic game page layout
    
    * Separate game page into smaller components
    
    * Fix incorrect merge
    
    * Add redirects file
    
    * Fix imports
    
    * Fix route bug
    
    * Add route verification
    
    * Add style to game controls and dice

---
+++ src/components/game/Players.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Players = () => {
+
+  return (
+    <div className={wrap}>ScrumMast3r vs _UXgUrU_</div>
+  )
+}
+
+const wrap = `
+  text-sm
+  text-center
+  mb-4
+`;
+
+export default Players;
