ecd446c971e9c33b4fe3169e30766d5ff3b2d179
Tis Lais <43742374+tislais@users.noreply.github.com>
2021-08-27 13:52:18 -0700

    Game page layout (#15)
    * Start adding elements to game page
    
    * Add basic game page layout
    
    * Separate game page into smaller components
    
    * Fix incorrect merge
    
    * Add redirects file
    
    * Fix imports
    
    * Fix route bug
    
    * Add route verification

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 // import { io } from 'socket.io-client';
 import Header from './components/header/Header';
 import AuthForm from './components/auth/AuthForm';
-import { Switch } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Lobby from './components/rooms/Lobby';
 import GameRoom from './components/rooms/GameRoom';
 import PrivateRoute from './components/common/PrivateRoute';
@@ -15,10 +15,10 @@ function App() {
       <Header />
 
       <Switch>
-        <PrivateRoute path="/" exact={true} component={AuthForm} />
+        <Route path="/" exact={true} component={AuthForm} />
         <SocketContext.Provider value={socket}>
-        <PrivateRoute path="/lobby" exact component={Lobby} />
-        <PrivateRoute path="/lobby/:room" exact component={GameRoom} />
+          <PrivateRoute path="/lobby" exact component={Lobby} />
+          <PrivateRoute path="/lobby/:room" exact component={GameRoom} />
         </SocketContext.Provider>
       </Switch>
     </>
