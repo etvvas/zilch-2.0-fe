cc5c48bfbad0aa32608c66cee3f03543fae02b1d
Tis Lais <43742374+tislais@users.noreply.github.com>
2021-08-30 15:09:29 -0700

    Modal work (#23)
    * Start adding elements to game page
    
    * Add basic game page layout
    
    * Separate game page into smaller components
    
    * Fix incorrect merge
    
    * Add redirects file
    
    * Fix imports
    
    * Fix route bug
    
    * Add route verification
    
    * Add style to game controls and dice
    
    * Refine game page UI
    
    * Fix breakpoint bug
    
    * Fix breakpoint bug again
    
    * Add font and change scoring options to checkboxes
    
    * Add dice roll animation css
    
    * Add style for disabled bank button
    
    * Add basic modal for rules
    
    * Remove unused variable
    
    * Adjust minor style all over
    
    * Remove extra variable declaration
    
    * WaitingRoom component
    
    * PlayerVersusPlayer component
    
    * add dice svg, noPlayerSvg styling
    
    * Add minor adjustments to waiting room
    
    * Separate rules and scoring into two modals
    
    * add results state to gameRoom and pass down to children, separate button from PlayerVersusPlayer
    
    * ResultsPage component
    
    * Results component
    
    * Start reformatting layout to allow for more buttons below game page
    
    * Tidy game options buttons
    
    * Comment out unused imports
    
    * Fix unused variable
    
    Co-authored-by: Katherine Tam <katherinemtam@gmail.com>

--- src/components/rooms/GameRoom.jsx
+++ src/components/rooms/GameRoom.jsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import ActiveScoreboard from '../game/ActiveScoreboard';
-import Dice from '../game/Dice';
-import GameControls from '../game/GameControls';
-import PlayerProgress from '../game/PlayerProgress';
+import React, { useState } from 'react';
+// import ActiveScoreboard from '../game/ActiveScoreboard';
+// import Dice from '../game/Dice';
+// import GameControls from '../game/GameControls';
+// import PlayerProgress from '../game/PlayerProgress';
 import Rules from '../game/Rules';
-import ScoringOptions from '../game/ScoringOptions';
+import Scoring from '../game/Scoring';
+// import ScoringOptions from '../game/ScoringOptions';
+import ResultsPage from '../results/ResultsPage';
+import WaitingRoom from './WaitingRoom';
 
 const GameRoom = () => {
-  
+  const [results, setResults] = useState(false);
+  setResults(false);
+
   return (
     <div className={main}>
 
       <div className={wrap}>
-        <PlayerProgress />
+        {!results ? <WaitingRoom results={results}/> : null}
+
+        {/* <PlayerProgress />
         <ActiveScoreboard />
         <Dice />
         <GameControls />
-        <ScoringOptions />
+        <ScoringOptions />*/}
+
       </div>
-      <Rules />
+
+      <div className={footer}>
+        <Rules />
+        <Scoring />
+        <button className={button}>Leave</button>
+      </div>
+
+      {results ? <ResultsPage results={results} /> : null}
     </div>
   )
 }
@@ -32,6 +47,7 @@ const main = `
 
 const wrap = `
   max-w-screen-sm
+  w-full
   mx-auto
   p-4
   bg-white
@@ -39,6 +55,28 @@ const wrap = `
   sm:rounded-lg
   sm:my-8
   sm:p-12
+`;
+
+const footer = `
+  max-w-screen-sm
+  flex
+  flex-row
+  gap-2
+  sm:gap-4
+  justify-center
+`;
+
+const button = `
+  text-indigo-500
+  bg-gray-100
+  sm:bg-purple-400
+  sm:bg-opacity-50
+  sm:text-white
+  rounded-full
+  py-1
+  px-8
+  my-4
+  w-max
 `;
 
 export default GameRoom;
