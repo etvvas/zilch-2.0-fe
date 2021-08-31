8c0af83baada26d4fef5fb3780a160bd4aea4dca
Katherine Tam <katherinemtam@gmail.com>
2021-08-31 14:21:32 -0700

    display player zilches

--- src/components/profile/PlayerProfile.jsx
+++ src/components/profile/PlayerProfile.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useSession } from '../../state/SessionProvider';
 import avatars from '../../assets/avatars.svg';
 import { useParams } from 'react-router-dom';
-import { getPlayerGames, getPlayerWins, getUser } from '../../services/profile';
+import { getPlayerGames, getPlayerWins, getPlayerZilches, getUser } from '../../services/profile';
 
 const PlayerProfile = () => {
   const session = useSession();
@@ -18,12 +18,37 @@ const PlayerProfile = () => {
   useEffect(async() => {
     const fetchedUser = await getUser(username);
     setUser(fetchedUser);
+    
     // const fetchedWins = await getPlayerWins(user.userId)
     const fetchedWins = [...Array(6)]
     setWins(fetchedWins.length);
+    
     // const fetchedGames = await getPlayerGames(user.userId);
     const fetchedGames = [...Array(18)]
     setLosses(fetchedGames.length - wins);
+    
+    // const fetchedZilches = await getPlayerZilches(user.userId);
+    const fetchedZilches = [
+      {
+        userId: '1',
+        gameId: '1',
+        playerZilches: 2
+      },
+      {
+        userId: '1',
+        gameId: '2',
+        playerZilches: 1
+      },
+      {
+        userId: '1',
+        gameId: '3',
+        playerZilches: 20
+      }
+    ]
+    const allZilches = fetchedZilches.reduce((a, b) => ({
+      playerZilches: a.playerZilches + b.playerZilches
+    }))
+    setZilches(allZilches.playerZilches)
   }, [])
 
   return (
@@ -36,7 +61,7 @@ const PlayerProfile = () => {
       <h2>user: {user.username}</h2>
       <h2>wins: {wins}</h2>
       <h2>losses: {losses} </h2>
-      <h2>zilches: </h2>
+      <h2>zilches: {zilches} </h2>
       <h2>uberZilches: </h2>
 
     </div>
