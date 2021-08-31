25647ad1f6e190c58eee42f355cab40df4458ec6
Tis Lais <tistististis@gmail.com>
2021-08-31 11:01:44 -0700

    Change room names

--- src/components/common/PlayerVersusPlayer.jsx
+++ src/components/common/PlayerVersusPlayer.jsx
@@ -2,22 +2,23 @@ import React from 'react';
 import avatars from '../../assets/avatars.svg';
 
 const PlayerVersusPlayer = ({user1, user2, room, results}) => {
-  room = 'Room1';
+  room = 'Titanium';
   const avatar = 'dice';
   // add conditional styling (noPlayerSvg) if player does not exist
 console.log('results', results)
   return (
     <>
       <div className={outerWrap}>
-        <h3 className={h3}>{room}</h3>
+        <h3 className={h3}>{room} Room</h3>
+        </div>
         <div className={innerWrap}>
           <div className={side}>
-            <p className={playerName}>Player 1</p>
+            <p className={playerName + playerOne}>ScrumMast3r</p>
             <svg className={noPlayerSvg + winning}>
               <use href={avatars + `#${avatar}`} />
             </svg>
             {!results  
-              ? <p className={ready}>Ready!</p>
+              ? <p className={ready}>Ready</p>
               : null
             }
           </div>
@@ -25,30 +26,22 @@ console.log('results', results)
             vs
           </div>
           <div className={side}>
-            <p className={playerName}>Player 2</p>
+            <p className={playerName + playerTwo}>_UXgUrU_</p>
             <svg className={svg}>
               <use href={avatars + `#${avatar}`} />
             </svg>
             {!results  
-              ? <p className={ready}>Ready!</p>
+              ? <p className={ready}>Ready</p>
               : null
             }
           </div>
-        </div>
+      </div>
         <div className={readyMessage}>
-          {/* Case1: player joins, no other player has joined yet
-            "waiting for opponent to join"
-          */}
-          {/* Case2: player joins and hits ready button 
-            "waiting on opponent to be ready"
-          */}
-          {/*  */}
           {!results  
               ? 'Game will start when both players are ready.'
               : null
           }
         </div>
-      </div>
     </>
   )
 }
@@ -62,11 +55,12 @@ const outerWrap = `
 `;
 
 const h3 = `
-  text-lg
+  text-2xl
   text-center
   p-2
-  text-gray-300
-  tracking-wider
+  text-indigo-500
+  font-bold
+  tracking-wide
   rounded-t-lg
 `;
 
@@ -106,8 +100,8 @@ const vs = `
 `;
 
 const svg = `
-  w-12
-  h-12
+  w-20
+  h-20
   md:w-12
   md:h-12
   my-2
@@ -117,14 +111,12 @@ const svg = `
 `;
 
 const noPlayerSvg = `
-  w-12
-  h-12
+  w-20
+  h-20
   md:w-12
   md:h-12
   my-2
   mx-auto
-  border-4
-  border-transparent
   filter
   grayscale
 `;
@@ -138,6 +130,18 @@ const ready = `
 
 const readyMessage = `
   text-center
+  text-sm
+  text-gray-700
+  border-b
+  italic
+`;
+
+const playerOne = `
+  text-purple-500
+`;
+
+const playerTwo = `
+  text-pink-500
 `;
 
 export default PlayerVersusPlayer;
--- src/components/rooms/Lobby.jsx
+++ src/components/rooms/Lobby.jsx
@@ -5,7 +5,7 @@ import Room from './Room';
 
 const Lobby = () => {
 
-  const rooms = ['Room 1', 'Room 2', 'Room 3', 'Room 4', 'Room 5', 'Room 6'];
+  const rooms = ['Vibranium', 'Gold', 'Xenon', 'Mythril', 'Titanium', 'Adamantium'];
 
 
   const roomsElements = rooms.map((room) => (
