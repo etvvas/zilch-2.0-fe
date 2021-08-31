900e467b5422578d24199d37f7947d98683fc6e9
Katherine Tam <katherinemtam@gmail.com>
2021-08-31 14:04:21 -0700

    getPlayerGames function

--- src/services/profile.js
+++ src/services/profile.js
@@ -6,10 +6,21 @@ export const getUser = async (username) => {
 }
 
 export const getPlayerWins = async (id) => {
-  console.log('id', id)
   const wins = await fetch(`${process.env.REACT_APP_LOCAL_URL}/api/v1/users/${id}/wins`)
-  const json = wins.json()
   
-  console.log('wins', json);
+  const json = wins.json()
   return json;  
 }
+
+//player losses - get all games by a user => length - wins; /api/v1/users/user_id/games
+export const getPlayerGames = async (id) => {
+  const games = await fetch(`${process.env.REACT_APP_LOCAL_URL}/api/v1/users/${id}/games`)
+  
+  const json = games.json()
+  console.log('losses', json)
+  return json; 
+}
+
+//player zilches - /api/v1/users/user_id/zilches
+
+//player uberZilches - /api/v1/users/user_id/uberZilches
