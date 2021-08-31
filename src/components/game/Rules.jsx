471a805136ff61b01b0f2f96399d8e06c3fa741a
Tis Lais <43742374+tislais@users.noreply.github.com>
2021-08-30 20:49:36 -0700

    Scoring modal style (#29)
    * Start adding elements to game page
    
    * Add basic game page layout
    
    * Separate game page into smaller components
    
    * Fix incorrect merge
    
    * Add redirects file
    
    * Fix imports
    
    * Fix route bug
    
    * Add route verification
    
    * Add style to game controls and dice
    
    * Refine game page UI
    
    * Fix breakpoint bug
    
    * Fix breakpoint bug again
    
    * Add font and change scoring options to checkboxes
    
    * Add dice roll animation css
    
    * Add style for disabled bank button
    
    * Add basic modal for rules
    
    * Remove unused variable
    
    * Adjust minor style all over
    
    * Remove extra variable declaration
    
    * WaitingRoom component
    
    * PlayerVersusPlayer component
    
    * add dice svg, noPlayerSvg styling
    
    * Add minor adjustments to waiting room
    
    * Separate rules and scoring into two modals
    
    * add results state to gameRoom and pass down to children, separate button from PlayerVersusPlayer
    
    * ResultsPage component
    
    * Results component
    
    * Start reformatting layout to allow for more buttons below game page
    
    * Tidy game options buttons
    
    * Comment out unused imports
    
    * Fix unused variable
    
    * Fix state bug
    
    * Fix overflow scroll bug in modals
    
    * Style scoring modal content and fix remaining modal layout bugs
    
    Co-authored-by: Katherine Tam <katherinemtam@gmail.com>

--- src/components/game/Rules.jsx
+++ src/components/game/Rules.jsx
@@ -82,6 +82,8 @@ const modalWrap = `
   bg-white
   rounded
   pt-12
+  h-full
+  sm:h-auto
 `;
 
 const modalInner = `
@@ -104,13 +106,15 @@ const x = `
 const h2 = `
   text-2xl
   text-center
+  text-indigo-700
   font-black
   mt-8
 `;
 
 const h3 = `
-  text-lg
-  font-bold
+  text-md
+  sm:text-lg
+  font-extrabold
   mt-4
   mb-1
 `;
