a6b588f2e014a346c66c9bc41a262e38106f3803
Chase <cabbott93@gmail.com>
2021-08-30 17:48:51 -0700

    add heroku url to socket porivder

--- src/state/SocketProvider.jsx
+++ src/state/SocketProvider.jsx
@@ -3,7 +3,7 @@ import io from 'socket.io-client'
 export const SocketContext = createContext();
 
 export default function SocketProvider({children}) {
-const socket = io('http://localhost:7890')
+const socket = io(process.env.REACT_APP_HEROKU_URL)
     return (
         <SocketContext.Provider value={socket}>
             {children}
