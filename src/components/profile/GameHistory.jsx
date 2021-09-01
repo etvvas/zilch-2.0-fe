5492a66165add34363336faf367fbcf8c920c84c
Katherine Tam <katherinemtam@gmail.com>
2021-09-01 16:16:10 -0700

    comment out items not being used

--- src/components/profile/GameHistory.jsx
+++ src/components/profile/GameHistory.jsx
@@ -4,7 +4,7 @@ import { getUserById } from '../../utils/profile';
 const GameHistory = ({games}) => {
   console.log('games', games)
 
-  useEffect(() => {
+  // useEffect(() => {
     // fetch by id (user1, user2)
       //render username and avatar
     // fetch results by gameId
@@ -13,13 +13,13 @@ const GameHistory = ({games}) => {
       // compare games to userId && gameId
     // fetch uberzilches by gameId
       // compare games to userId && gameId
-  })
+  // })
  
 
 
-  const gameHistory = games.map(async (game, i)=> {
-    const user1 = await getUserById(game.firstUserId)
-    const user2 = await getUserById(game.secondUserId)
+  // const gameHistory = games.map(async (game, i)=> {
+  //   const user1 = await getUserById(game.firstUserId)
+  //   const user2 = await getUserById(game.secondUserId)
     
     return (
       <li key={i}>
