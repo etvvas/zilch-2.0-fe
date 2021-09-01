74e6cc6f85d9ae7e498804c4a3c91bd1581ff87a
Katherine Tam <katherinemtam@gmail.com>
2021-08-31 20:45:15 -0700

    getPlayerWins function

--- src/utils/profile.js
+++ src/utils/profile.js
@@ -2,6 +2,12 @@ export const getUser = async (username) => {
   const user = await fetch(`${process.env.REACT_APP_HEROKU_URL}/api/v1/users/${username}`)
 
   const json = user.json()
-  console.log('json', json)
   return json;
+}
+
+export const getPlayerWins = async (id) => {
+  const wins = await fetch(`${process.env.REACT_APP_HEROKU_URL}/api/v1/users/${id}/wins`)
+  
+  const json = wins.json()
+  return json;  
 }
