38e3db8f182bb543ad8062d5e7d99ca544614838
Joseph Davis <joethebartenderpdx@gmail.com>
2021-09-01 10:03:31 -0700

    changed env to localhost

--- src/utils/profile.js
+++ src/utils/profile.js
@@ -1,33 +1,33 @@
 export const getUser = async (username) => {
-  const user = await fetch(`${process.env.REACT_APP_HEROKU_URL}/api/v1/users/${username}`)
+  const user = await fetch(`${process.env.REACT_APP_LOCAL_URL}/api/v1/users/${username}`)
 
   const json = user.json()
   return json;
 }
 
 export const getPlayerWins = async (id) => {
-  const wins = await fetch(`${process.env.REACT_APP_HEROKU_URL}/api/v1/users/${id}/wins`)
+  const wins = await fetch(`${process.env.REACT_APP_LOCAL_URL}/api/v1/users/${id}/wins`)
   
   const json = wins.json()
   return json;  
 }
 
 export const getPlayerGames = async (id) => {
-  const games = await fetch(`${process.env.REACT_APP_HEROKU_URL}/api/v1/users/${id}/games`)
+  const games = await fetch(`${process.env.REACT_APP_LOCAL_URL}/api/v1/users/${id}/games`)
   
   const json = games.json()
   return json; 
 }
 
 export const getPlayerZilches = async (id) => {
-  const zilches = await fetch(`${process.env.REACT_APP_HEROKU_URL}/api/v1/users/${id}/zilches`)
+  const zilches = await fetch(`${process.env.REACT_APP_LOCAL_URL}/api/v1/users/${id}/zilches`)
   
   const json = zilches.json()
   return json; 
 }
 
 export const getPlayerUberZilches = async (id) => {
-  const uberZilches = await fetch(`${process.env.REACT_APP_HEROKU_URL}/api/v1/users/${id}/uberZilches`)
+  const uberZilches = await fetch(`${process.env.REACT_APP_LOCAL_URL}/api/v1/users/${id}/uberZilches`)
   
   const json = uberZilches.json()
   console.log('uber', json)
