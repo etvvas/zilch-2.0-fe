6fbdfdd45ae331cd8c754ae39bdbc0ec8c3188df
Joseph Davis <joethebartenderpdx@gmail.com>
2021-09-01 13:57:21 -0700

    returning username and total wins from most to least

--- src/state/SocketProvider.jsx
+++ src/state/SocketProvider.jsx
@@ -4,7 +4,7 @@ import io from 'socket.io-client'
 export const SocketContext = createContext();
 
 export default function SocketProvider({children}) {
-const socket = io(process.env.REACT_APP_HEROKU_URL)
+const socket = io(process.env.REACT_APP_LOCAL_URL)
     return (
         <SocketContext.Provider value={socket}>
             {children}
