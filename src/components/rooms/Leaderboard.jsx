6de18367487c9c19eb25bbbb1789bf4f75ba6b13
Joseph Davis <joethebartenderpdx@gmail.com>
2021-09-01 14:20:17 -0700

    setting leaderboard into state

--- src/components/rooms/Leaderboard.jsx
+++ src/components/rooms/Leaderboard.jsx
@@ -3,23 +3,21 @@ import React, { useState, useEffect} from 'react';
 import { displayLeaders, displayWins, getUsers, getWins, orderWins } from '../../services/users';
 
 const Leaderboard = () => {
-    const [users, setUsers] = useState([]);
-    const [winners, setWinners] = useState([])
+    const [leaderboard, setLeaderboard] = useState([]);
 
     useEffect(() => {
         const placeholderFunc = async () => {
             const users = await getUsers();
             const wins = await getWins(users)
-            const array = await orderWins(wins)
-            const leaders = await displayLeaders(array)
+            const order = await orderWins(wins)
+            const leaders = await displayLeaders(order)
+            await setLeaderboard(leaders)
             console.log(leaders)
         }
         return placeholderFunc()
     }, [])
 
-    return <h1>Leaderboard</h1>
-
-
+    return <h1>leaderboard</h1>
+    
 }
-
 export default Leaderboard
