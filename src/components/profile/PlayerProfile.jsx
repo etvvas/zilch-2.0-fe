085a790df279dcd6297f71f97b0b703c0906bbe5
Katherine Tam <katherinemtam@gmail.com>
2021-09-01 13:44:05 -0700

    fix reduce functions, get player profiles rendering on page load

--- src/components/profile/PlayerProfile.jsx
+++ src/components/profile/PlayerProfile.jsx
@@ -7,8 +7,8 @@ import GameHistory from './GameHistory';
 
 const PlayerProfile = () => {
   const {username} = useParams();
-  console.log(username)
 
+  const [loading, setLoading] = useState(true);
   const [user, setUser] = useState({});
   const [wins, setWins] = useState(0);
   const [games, setGames] = useState([])
@@ -16,52 +16,61 @@ const PlayerProfile = () => {
   const [zilches, setZilches] = useState(0);
   const [uberZilches, setUberZilches] = useState(0);
 
-  useEffect(async() => {
-    const fetchedUser = await getUser(username);
+  useEffect(() => {
+
+    const holder = async() => {
+    const fetchedUser = await getUser(username)
     setUser(fetchedUser);
 
-    const fetchedWins = await getPlayerWins(user.userId)
-    console.log(fetchedWins)
+    const fetchedWins = await getPlayerWins(fetchedUser.userId)
+    console.log('wins', fetchedWins)
     setWins(fetchedWins.length);
     
-    const fetchedGames = await getPlayerGames(user.userId);
+    const fetchedGames = await getPlayerGames(fetchedUser.userId);
     setGames(fetchedGames);
     const losses = fetchedGames.length - wins 
-    console.log(losses)
+    console.log('losses', losses)
     setLosses(losses);
     
-    const fetchedZilches = await getPlayerZilches(user.userId);
-    const allZilches = Array.from(fetchedZilches).reduce((a, b) => a.playerZilches + b.playerZilches, 0)
-    console.log(allZilches)
+    const fetchedZilches = await getPlayerZilches(fetchedUser.userId);
+    const allZilches = fetchedZilches.reduce((a, b) => a + b.playerZilches, 0)
     setZilches(allZilches)
 
     
-    const fetchedUberZilches = await getPlayerUberZilches(user.userId)
-    const allUberZilches = Array.from(fetchedUberZilches).reduce((a, b) => a.playerUberZilches + b.playerUberZilches, 0)
-    console.log(allUberZilches)
+    const fetchedUberZilches = await getPlayerUberZilches(fetchedUser.userId)
+    const allUberZilches = fetchedUberZilches.reduce((a, b) => a + b.playerUberZilches, 0)
     setUberZilches(allUberZilches)
+
+    setLoading(false);
+    }
+
+    holder()
   }, [])
 
   // stats renders sometimes; not enough time to fetch
 
   // global rank = take leaderboard (wins), match userId, and get index + 1
-
   return (
-    <>
-    <div className={profile}>
-      <h1>{user.username}'s Profile</h1>
-      <div>
-        <svg className={svg}>
-          <use href={avatars + `#${user.avatar}`} />
-        </svg>
-        <h2>Global Rank #</h2>
-        <PlayerStats user={user} wins={wins} losses={losses} zilches={zilches} uberZilches={uberZilches}/>
+    <div>
+    {loading 
+      ? <h1>Loading...</h1> 
+      : <>
+        <div className={profile}>
+          <h1>{user.username}'s Profile</h1>
+          <div>
+            <svg className={svg}>
+              <use href={avatars + `#${user.avatar}`} />
+            </svg>
+            <h2>Global Rank #</h2>
+            <PlayerStats user={user} wins={wins} losses={losses} zilches={zilches} uberZilches={uberZilches}/>
 
-        <GameHistory games={games}/>
-        
-      </div>
-      </div>
-    </>
+            <GameHistory games={games}/>
+            
+          </div>
+          </div>
+        </>
+    }
+    </div>
   )
 }
 
