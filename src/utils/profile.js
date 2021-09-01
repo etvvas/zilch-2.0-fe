1a2eade66fd18848adc48a46a58dc301645134dc
Katherine Tam <katherinemtam@gmail.com>
2021-08-31 22:01:37 -0700

    getUserById function

--- src/utils/profile.js
+++ src/utils/profile.js
@@ -20,14 +20,22 @@ export const getPlayerGames = async (id) => {
 }
 
 export const getPlayerZilches = async (id) => {
-  const zilches = await fetch(`${process.env.REACT_APP_LOCAL_URL}/api/v1/users/${id}/zilches`)
+  const zilches = await fetch(`${process.env.REACT_APP_HEROKU_URL}/api/v1/users/${id}/zilches`)
   
   const json = zilches.json()
   return json; 
 }
 
 export const getPlayerUberZilches = async (id) => {
-  const uberZilches = await fetch(`${process.env.REACT_APP_LOCAL_URL}/api/v1/users/${id}/uberZilches`)
+  const uberZilches = await fetch(`${process.env.REACT_APP_HEROKU_URL}/api/v1/users/${id}/uberZilches`)
+  
+  const json = uberZilches.json()
+  console.log('uber', json)
+  return json; 
+}
+
+export const getUserById = async (id) => {
+  const uberZilches = await fetch(`${process.env.REACT_APP_LOCAL_URL}/api/v1/users/id/${id}`)
   
   const json = uberZilches.json()
   return json; 
