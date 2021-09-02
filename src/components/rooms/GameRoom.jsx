5b098ebf77e245aa4d694b4fd09fcea9fdcd724e
Gabriel Simek <71483160+gabrielsimek@users.noreply.github.com>
2021-09-01 17:25:30 -0700

    Round scores (#57)
    * add console logs
    
    * remove unused var
    
    * merge with dev
    
    * merged changes
    
    * change to react app url
    
    * HOTFIX/ set isFreeRoll false on bank
    
    * add loading on gameRoom
    
    * display round score tables
    
    * HOTFIX/ choices matched on choice not index, fixes 1 and 5 problem

--- src/components/rooms/GameRoom.jsx
+++ src/components/rooms/GameRoom.jsx
@@ -88,10 +88,9 @@ const GameRoom = () => {
       setDice(dice);
       setTimeout(() => {
         setScoringOptions(
-          scoringOptions.map((option, i) => {
+          scoringOptions.map((option) => {
             return {
               ...option,
-              id: i,
               selected: false,
             };
           })
@@ -158,7 +157,7 @@ const GameRoom = () => {
 
   const handleScoreSelect = ({ target }) => {
     const updatedScoringOptions = scoringOptions.map((option) => {
-      if (option.id === JSON.parse(target.value).id)
+      if (option.choice === JSON.parse(target.value).choice)
         return { ...option, selected: true };
       else return option;
     });
