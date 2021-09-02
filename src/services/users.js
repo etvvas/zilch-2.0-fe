9448826fcc39edc4d66a1aa87c3643078116b1e1
Katherine Tam <74214477+katherinemtam@users.noreply.github.com>
2021-09-02 13:43:24 -0700

    GameHistory (#69)
    * if cookie, no login page
    
    * loading moved from PrivateRoute to AuthForm, displys once
    
    * deleted console logs
    
    * logout button setSession to null, redirects to authForm
    
    * slightly cleaner handleLogout
    
    * added confirm to logout
    
    * loading displays long enough for animations to matter
    
    * getUsers and getWins function in Leaderboard
    
    * getUsers works, getWins gets users won games but still as a promise
    
    * Finally, getting users wins as array
    
    * console log removed, useState brought in
    
    * password type added, unused useEffect
    
    * changed to heroku url
    
    * accepted changes
    
    * add profile route
    
    * add profile navigation
    
    * organize imports
    
    * getUser function
    
    * move over code from player-profile branch, display user
    
    * getPlayerWins function
    
    * display player avatar and wins
    
    * getPlayerGames function
    
    * display player losses
    
    * getPlayerZilches function
    
    * display player zilches
    
    * getPlayerUberZilches function
    
    * display player uber zilches
    
    * PlayerStats component
    
    * fix reduce error, pass props down to PlayerStats
    
    * add lobby navigation
    
    * GameHistory component
    
    * console.logs checking render
    
    * getUserById function
    
    * pass gameState props to WaitingRoom and PlayerVersusPlayer
    
    * pass gameState props to ResultsPage
    
    * render results
    
    * pass winner gameState to render in results
    
    * rename profile folder, add background to profile
    
    * add comments to PlayerProfile - global rank and fetch delay
    
    * add comments to approach GameHistory
    
    * merged with dev
    
    * changed env to localhost
    
    * add loading conditional to private route
    
    * remove console log
    
    * fix reduce functions, get player profiles rendering on page load
    
    * returning username and total wins from most to least
    
    * setting leaderboard into state
    
    * switched socketprovider back to heroku
    
    * added required for username/password, close hanburger menu on click
    
    * switched from local to heroku again
    
    * fixed env file
    
    * env slash
    
    * new styling pulled
    
    * add promise and adjust comments
    
    * comment out items not being used
    
    * comment out more items
    
    * dynamic leaderboard
    
    * fix form avatar selection value
    
    * GameHistory structure
    
    * game stats functions
    
    * pass user prop to GameHistory
    
    * gameHistory planning, api functions commented out
    
    * add possible Promise.all code to fix error
    
    * console log removed, loading state in leaderboard
    
    * refactored leaderboard funcs
    
    * fix indenting, remove unused imports
    
    * display player's global rank
    
    * comment back in GameHistory logic, render stats
    
    * fix uberZilches render
    
    * display player avatar in Game History
    
    * getWinner function
    
    * display game results
    
    * display targetScore
    
    * remove console logs
    
    Co-authored-by: Joseph Davis <joethebartenderpdx@gmail.com>

--- src/services/users.js
+++ src/services/users.js
@@ -14,19 +14,49 @@ export const getWins =  async (idArray) => {
 }
 
 
+export const getWinsArray = async () => {
+            const users = await getUsers();
+            const wins = await getWins(users);
+            return Promise.all(wins);
+        }
+
 export const orderWins = async (winsArray) => {
-  const noneZeroWinners = winsArray.filter(n => n.length)
-  const totalWins = noneZeroWinners.map(wins => wins)
-  return totalWins.sort(function(a, b){
+  const nonZeroWinners = winsArray.filter(n => n.length)
+  return nonZeroWinners.sort(function(a, b){
     if (a > b) return -1;
     if (a < b) return 1;
     return 0
   })
 }
 
+export const allLeaders = async () => {
+  const wins = await getWinsArray();
+  const order = await orderWins(wins)
+  const leaders = await displayLeaders(order)
+  return leaders
+}
+
+// export const display = async (array) => {
+//   const noZeroLengths = array.filter(n => n.length)
+//   const sorted = noZeroLengths.sort(function(a, b){
+//     if (a > b) return -1;
+//     if (a < b) return 1;
+//     return 0
+//   })
+//   const leaders = sorted.map((wins, i) => (
+//      {
+//       userId: wins[0].userId,
+//       username: wins[0].username,
+//       wins: wins.length
+//     }
+//   ))
+//   return leaders
+// }
+
 export const displayLeaders = async (totalWins) => {
   const leaders = totalWins.map((wins, i) => (
     {
+      userId: wins[0].userId,
       username: wins[0].username,
       wins: wins.length
     }
